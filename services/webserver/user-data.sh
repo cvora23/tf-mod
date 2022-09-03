6113c10d6942256806fe95046dd65c0c17ef435b
Chintan Vora <chintan.vora@ubyon.com>
2022-09-03 16:30:24 -0700

    User Data Nginx Docker

--- services/webserver/user-data.sh
+++ services/webserver/user-data.sh
@@ -1,11 +1,10 @@
 #! /bin/bash
 
-sudo apt update
-sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+sudo apt update -y
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable"
-sudo apt update
-sudo apt-get install docker-ce
+sudo add-apt-repository --yes "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable"
+sudo apt-get install -y docker-ce
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo groupadd docker
