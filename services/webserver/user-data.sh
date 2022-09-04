09478d511b0b7b7db3b7cc513a54c1187c0c21ad
Chintan Vora <chintan.vora@ubyon.com>
2022-09-03 17:10:11 -0700

    user-data update

--- services/webserver/user-data.sh
+++ services/webserver/user-data.sh
@@ -11,11 +11,3 @@ sudo groupadd docker
 sudo usermod -aG docker ubuntu
 sudo docker pull nginx:latest
 sudo docker run --name mynginx1 -p 80:80 -d nginx
-
-#cat > index.html <<EOF
-#<h1>Hello, World</h1>
-#<p>VPC Security Group ID: ${vpc_security_group_ids}</p>
-#<p>App Server Subnet ID: ${subnet_id}</p>
-#EOF
-#
-#nohup busybox httpd -f -p ${server_port} &
