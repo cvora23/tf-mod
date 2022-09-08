89e139db823bdf84f1469854e42731224bb9837d
Chintan Vora <chintan.vora@ubyon.com>
2022-09-08 16:31:01 -0700

    gwlbe module

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -1,3 +1,13 @@
+output "vpc_id" {
+  value       = aws_vpc.svc_vpc.id
+  description = "The ID of the VPC"
+}
+
+output "igw_id" {
+  value       = aws_internet_gateway.igw.id
+  description = "The ID of the Internet GW"
+}
+
 output "vpc_security_group_id" {
   value       = aws_security_group.sg.id
   description = "The ID of the Security Group attached to the VPC"
