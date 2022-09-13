25b7e602c9216c3dc7ac83b53587d7f9b436fa87
Chintan Vora <chintan.vora@ubyon.com>
2022-09-13 10:47:19 -0700

    GWLBE module fixes

--- gwlbe/variables.tf
+++ gwlbe/variables.tf
@@ -23,16 +23,6 @@ variable "webserver_local_state_path" {
   type        = string
 }
 
-variable "gwlbe_cidr_block" {
-  description = "CIDR Block reserved for GW Load Balancer Endpoint"
-  type        = string
-}
-
-variable "app_cidr_block" {
-  description = "CIDR Block reserved for Application Server"
-  type        = string
-}
-
 variable "ubyon_gwlbe_svc_name" {
   description = "Ubyon GW Load Balancer Endpoint Service Name"
   type        = string
