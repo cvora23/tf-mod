1dc7de32668ad546d559097133df1a202c1e1b9c
Chintan Vora <chintan.vora@ubyon.com>
2022-09-12 17:40:53 -0700

    VPC and WebServer module fixes

--- services/webserver/outputs.tf
+++ services/webserver/outputs.tf
@@ -1,5 +1,5 @@
 output "app_server_public_ip" {
-  value = aws_instance.app_server[count.index].public_ip
+  value = aws_instance.app_server[*].public_ip
   description = "The Public IP of the app server"
 }
 
